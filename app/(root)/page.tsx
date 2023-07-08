9af2fa83593abbe4ae2f62124b8fa7c33b387c3c
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-07-09 01:04:39 +0800

    created modal for creating store

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default function SetuoPage() {
   return (
     <div className="p-4">
-      <Button size="default">hi</Button>
+      <UserButton afterSignOutUrl="/"/>
     </div>
   );
 }
